c966b1c2d99ea6b47722c0bbe4dc3595d97395e4
Shunya Yokoi <88517400+shun-1129@users.noreply.github.com>
2022-01-28 10:39:03 +0900

    UP

---
+++ target/masaru_system/js/costomer_in1.js
@@ -0,0 +1,3 @@
+setTimeout(function () {
+    window.location.href = "../customer_index.html";
+}, 30 * 1000);
---
+++ target/masaru_system/js/member_singup_conf.js
@@ -0,0 +1,10 @@
+$(function () {
+    $(".js_modal_open").on("click", function () {
+        $(".js_modal").fadeIn();
+        return false;
+    });
+    $(".js_modal_close").on("click", function () {
+        $(".js_modal").fadeIn();
+        return false;
+    });
+});
